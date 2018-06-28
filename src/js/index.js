bd59c0ba4a5487311f650fd8186d632bb0622a4c
Denis <den.neposeda@gmail.com>
2018-06-28 16:45:22 +0400

    fix adaptive amd menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
 import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap-grid.css';
 import '../css/style.css';
+
+
+var doc = document;
+var btnMenu = doc.getElementById('menu-btn');
+
+function showBtnMenu(){
+    var humbergerMenu = doc.getElementById('menu-humberger');
+    btnMenu.classList.toggle('menu-btn--active');
+    humbergerMenu.classList.toggle('menu__humberger--active');
+};
+
+btnMenu.onclick = showBtnMenu;
